4a3be2077d8e43f6273dd58d1bbae0ca8f361c45
Andrew <andrewmd4774@gmail.com>
2018-05-30 21:52:58 -0400

    game is restarting every time key is pressed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,41 @@
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var compLetter = Math.floor((Math.random()*letters.length) + 1);
+var wins = 0;
+var losses = 0;
+var numGuesses = 9;
+var lettersGuessed = [];
 
 document.onkeyup = game;
 
 function game() {
-    var key = String.fromCharCode(event.keyCode);
-    console.log(key);
+    var compLetter = letters[Math.floor((Math.random()*letters.length) + 1)];
+    console.log(compLetter);
+    var key = String.fromCharCode(event.keyCode).toLowerCase();
+    
+    if (key > "a" && key < "z") {
+        
+        if (key === compLetter) {
+            wins++;
+            document.getElementById("wins").innerHTML = "Wins: " + wins;
+            numGuesses = 9;
+            document.getElementById("guesses").innerHTML = "Remaining Guesses: " + numGuesses;
+            lettersGuessed = [];
+            document.getElementById("letters").innerHTML = "Letters Guessed: ";
+        }
+
+        else {
+            numGuesses--;
+            document.getElementById("guesses").innerHTML = "Remaining Guesses: " + numGuesses;
+            lettersGuessed.push(key);
+            document.getElementById("letters").innerHTML = "Letters Guessed: " + lettersGuessed;
+
+            if (numGuesses < 1) {
+                losses++;
+                document.getElementById("losses").innerHTML = "Losses: " + losses;
+                numGuesses = 9;
+                document.getElementById("guesses").innerHTML = "Remaining Guesses: " + numGuesses;
+                lettersGuessed = [];
+                document.getElementById("letters").innerHTML = "Letters Guessed: " + lettersGuessed;
+            }
+        }
+    }
 }
